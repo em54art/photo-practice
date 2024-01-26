fe3f1ae7d7459ecbc3493366cf7979edf353afba
Emily <130906954+em54art@users.noreply.github.com>
2024-01-26 14:09:21 +1100

    Update practice_customtk_images.py
    fix bugs, problems with batch list, remove arrows in beginning and end, image problem

--- practice_customtk_images.py
+++ practice_customtk_images.py
@@ -8,6 +8,23 @@ import os
 import re
 from tkinter import filedialog
 
+sqe_size = (30, 30)
+batch_size = 3
+current_index = 0
+current_findex = 0
+change_array = 0
+current_array = 0
+#image for slider, full array           
+img_paths =[]
+#array with imported from batch array
+img_batchlist = []
+file_format = ['.png','.jpg']
+appended_list = []
+check_array = []        
+final_array = []
+
+
+
 #-------------------root--------------------------------------------------
 root = customtkinter.CTk()
 
@@ -179,65 +196,98 @@ title_label1.grid(row = 4, column = 0, sticky='s')
 # title_bar.bind('<ButtonPress-1>', move_app)
 # title_bar.bind('<B1-Motion>', drag_app)
 
+#------------
+
+#back arrowL code
+def back(full_batchlist):
+    global current_index,current_array,current_findex,final_array
+    print('back')
+    # reduce current index
+    current_index -= 1
+    current_findex -= 1
+    
+    
+    # Wrap around to the last 
+    if current_array == 0 and current_index == -1:
+        current_array = len(full_batchlist) - 1
+        current_index = len(full_batchlist[-1]) -1
+        
+    if current_index < 0:
+        current_array -= 1
+        current_index = len(full_batchlist[current_array]) - 1
+    
+    if current_findex == 0:
+        destroylb()
+    
+    delete_arrowR()
+    img_load2(full_batchlist,check_array,appended_list)
+    image_show()
+    
+    
+#next arrowR code
+def forward(full_batchlist):
+    global current_index, current_array, current_findex,final_array
+    print('next')
+    # Increment current index
+    current_index += 1
+    current_findex += 1
+    
+    if current_index >= len(full_batchlist[current_array]):
+        current_array += 1
+        current_index = 0
+        
+
+    if current_array == len(full_batchlist):
+        current_array = 0
+        current_index = 0
+    
+    if current_findex == 1:
+        destroylb()
+    
+    arrow_buttonR()
+    img_load2(full_batchlist,check_array,appended_list)
+    image_show()
+    
+
+
+#seperate R and L insert the function
+def arrow_buttonR():
+    global arrow_imgR, arrow_imgL, button_AR, button_AL, my_label,photo_img1,photo_img2, resized_images
+    arrow_imgR =Image.open(r"python_pic\arrow0.png")
+    img_AR = arrow_imgR.resize(sqe_size)
+    photo_img1 = ImageTk.PhotoImage(img_AR)
+    
+    #arrow button
+    button_AR = Button(root, image= photo_img1, command= lambda:forward(img_batchlist), width=30, height=30, borderwidth=0, highlightthickness=0, bg="white")
+    button_AR.grid(row = 3, column = 2, padx = 50, pady =5,sticky='e')
+    
+    delete_arrowR()
 
 #-----------------arrow-------------------------
 def arrow_button():
     global arrow_imgR, arrow_imgL, button_AR, button_AL, my_label,photo_img1,photo_img2, resized_images
-    
-    #arrow image
-    arrow_imgR =Image.open(r"python_pic\arrow0.png")
+        #arrow image
     arrow_imgL =Image.open(r"python_pic\arrow1.png")
     
-    sqe_size = (30, 30)
-    img_AR = arrow_imgR.resize(sqe_size)
     img_AL = arrow_imgL.resize(sqe_size)
     
     #convert resize image to tkinter object
-    photo_img1 = ImageTk.PhotoImage(img_AR)
     photo_img2 = ImageTk.PhotoImage(img_AL)
     
-     #next arrowR code
-    def forward():
-        global current_index, button_AR, button_AL, my_label
-        
-        # Increment current index
-        current_index += 1
-        
-        # Wrap around to beginning if at end of list
-        if current_index >= len(resized_images):
-            current_index = 0
-        
-        # Update label with new image
-        my_label.grid_forget()
-        my_label = Label(image=resized_images[current_index], borderwidth=0, highlightbackground="white")
-        my_label.grid(row=2, column=2, sticky='ew')
-        
-    #back arrowL code
-    def back():
-        global current_index, button_AR, button_AL, my_label
-        
-        # reduce current index
-        current_index -= 1
-        
-        # Wrap around to beginning if at end of list
-        if current_index >= len(resized_images):
-            current_index = 0
-        
-        # Update label with new image
-        my_label.grid_forget()
-        my_label = Label(image=resized_images[current_index], borderwidth=0, highlightbackground="white")
-        my_label.grid(row=2, column=2, sticky='ew')
-        
-        current_index = resized_images.index(resized_images[current_index])
-        
-    #arrow button
-    button_AR = Button(root, image= photo_img1, command=forward, width=30, height=30, borderwidth=0, highlightthickness=0, bg="white")
-    button_AR.grid(row = 3, column = 2, padx = 50, pady =5,sticky='e')
+    # Update label with new image
+    my_label.grid_forget()
+    my_label = Label(image = final_array[current_findex], borderwidth=0, highlightbackground="white")
+    my_label.grid(row=2, column=2, sticky='ew')
+    
+    
 
-    button_AL = Button(root, image= photo_img2, command=back, width=30, height=30, borderwidth=0, highlightthickness=0, bg="white")
+    button_AL = Button(root, image= photo_img2, command=lambda: back(img_batchlist), width=30, height=30, borderwidth=0, highlightthickness=0, bg="white")
     button_AL.grid(row = 3, column = 2, padx = 50, pady =5, sticky='w')
     
 
+    destroylb()
+    
+    
 #-------image-------
 def image_function():
     #use global to allow access
@@ -273,7 +323,7 @@ def image_function():
     #color button
     button_CP = Button(root, image= photo_img3, command=colorp_click, width=30, height=30, borderwidth=0, highlightthickness=0, bg="white")
     button_CP.grid(row = 1, column = 2,pady = 5, padx = 5, sticky='ne')
-    return resized_images
+    return final_array
 
 
 DragAndDrop_enabled= None
@@ -350,6 +400,8 @@ def example():
 
 
 #--------------file pathing-----------------
+
+
 #create file name
 dirname = os.path.dirname(__file__)
 filename = os.path.join(dirname,'default_pics')
@@ -378,10 +430,48 @@ def checkFolderDir():
 
 #function
 checkFolderDir()
+#when going the back array the current array isn't equal to appendedlist
+def image_insert(array,flat):
+    global current_array, current_findex
+    
+    #try appendedlist[current array]
+    for path in flat[current_array]:
+        img = Image.open(path)
 
-#image for slider            
-img_paths =[]
-file_format = ['.png','.jpg']
+        # Calculate aspect ratio
+        width_ratio = target_size[0]/img.width
+        height_ratio = target_size[1]/img.height
+        min_ratio = min(width_ratio, height_ratio)
+        
+        # Compute new dimensions
+        new_width = int(img.width * min_ratio)
+        new_height = int(img.height * min_ratio)
+
+        # Resize image
+        img = img.resize((new_width, new_height))
+        
+        #insert into array
+        array.append(ImageTk.PhotoImage(img))
+        print('insert')
+    
+    
+    
+
+#shitty batch array
+def batch_process(array, batch_size):
+    batch = []
+    
+    for item in array:
+        batch.append(item)
+        
+        if len(batch) == batch_size:
+            #returns set of values only need to read once
+            yield batch
+            batch = []
+    #if any item left in array, yield
+    if batch:
+        yield batch
+
 
 #inserting file names into array
 #read notepad
@@ -403,44 +493,90 @@ with open(direc,"r") as directname:
         else:
             print(f"Directory {folder} does not exist")
 
-# Initialize current index to 0
-current_index = 0
+def batch_processing():
+    #shove batch in imgbatchlist
+    for batch in batch_process(img_paths,batch_size):
+        img_batchlist.append(batch)
 
-def image_insert(array):
+batch_processing()
+
+def delete_arrowR():
+    global button_AR,final_length ,final_batch,batch_math
+    final_length = len(img_batchlist)
+    final_batch = len(img_batchlist[-1])
+    batch_math = batch_size * final_length - batch_size + (final_batch - 1)
+
+    if current_findex == batch_math:
+        print('True2')
+        return button_AR.destroy()
+    else:
+        print('false')
+        button_AR = Button(root, image= photo_img1, command= lambda:forward(img_batchlist), width=30, height=30, borderwidth=0, highlightthickness=0, bg="white")
+        button_AR.grid(row = 3, column = 2, padx = 50, pady =5,sticky='e')
+
+
+def destroylb():
+    global button_AL
     
-    for path in img_paths:
-        img = Image.open(path)
+    #destroy only on the first image, keep bool
+    if current_findex == 0:
+        print('true')
+        button_AL.destroy()
+    else:
+        print('false')
+        button_AL = Button(root, image= photo_img2, command=lambda: back(img_batchlist), width=30, height=30, borderwidth=0, highlightthickness=0, bg="white")
+        button_AL.grid(row = 3, column = 2, padx = 50, pady =5, sticky='w')
 
-        # Calculate aspect ratio
-        width_ratio = target_size[0]/img.width
-        height_ratio = target_size[1]/img.height
-        min_ratio = min(width_ratio, height_ratio)
-        
-        # Compute new dimensions
-        new_width = int(img.width * min_ratio)
-        new_height = int(img.height * min_ratio)
 
-        # Resize image
-        img = img.resize((new_width, new_height))
-        
-        #insert into array
-        array.append(ImageTk.PhotoImage(img))
 
+img_loaded =False 
+#load first batch (image insert array is used on the first batch)
+def img_load1(full_batchlist,check_array,appended_list):
+    global final_array, img_loaded
+    img_loaded =True 
+    appended_list.append(full_batchlist[current_array])
+    check_array.append(current_array)
+    image_insert(final_array,appended_list)
+    
     
 
 
+
+img_load1(img_batchlist,check_array,appended_list)
+
+
+def img_load2(full_batchlist, check_array,appended_list):
+    global change_array, final_array, current_array, img_loaded
+    img_loaded =False 
+
+    print(f'current:{current_array} change: {change_array}')
+    
+    #if current array changes load in the array appended_list(check what is happening)
+    if current_array != change_array:
+        if current_array in check_array:
+            print('do nothing')
+        else:
+            
+            #append to appendedlist
+            appended_list.append(full_batchlist[current_array])
+            #append number to check
+            check_array.append(current_array)
+            #insert the appended_list again
+            image_insert(final_array,appended_list)
+            
+    else:
+
+        print('not changed')
 
 # Function for opening the file explorer window
 listchange = False
 
 def browseFiles():
-    global DragAndDrop_enabled, resized_images,strFolder
-    resized_images = []
-    
+    global DragAndDrop_enabled, final_array,strFolder,check_array,appended_list
+    global img_batchlist,img_paths,final_array,current_findex
     DragAndDrop_enabled = None
     listchange= True 
     filename = filedialog.askdirectory()
-    
     
     if filename == '':
         #read inside notepad
@@ -452,9 +588,17 @@ def browseFiles():
             myFile.write(filename)
         strFolder = filename
     
-    if len(img_paths) > 0 :
+    if len(final_array) > 0 :
+        final_array.clear()
         img_paths.clear()
-    
+        img_batchlist.clear()
+        appended_list.clear()
+        check_array.clear()   
+        final_array.clear()
+        
+    else:
+        print('not clear')
+        
     #goes through the directory
     for root, dirs, files in os.walk(strFolder):
         for file in files:
@@ -469,18 +613,22 @@ def browseFiles():
             #checks if its in file_format. if not continue without executing
             if fileType not in file_format:
                 continue
-            #if it is continue appending img_path called in insert
+            #if it is continue appending final array called in insert
             img_paths.append(normalise)
     
-    image_insert(resized_images)
+    #does the data processing again
+    batch_processing()
+    img_load1(img_batchlist,check_array,appended_list)
+    #image process
+    image_insert(final_array,appended_list)
+    if current_findex > 0:
+        current_findex = 0
     destroy_browsing()
+    image_show()
 
 
 def image_show():
-    global my_label,my_label0,frame1
-    #func
-    image_insert(resized_images)
-
+    global my_label,my_label0,frame1,final_array,current_findex
     #image
     my_img0 =Image.open(r"python_pic\background.png")
     img5 = my_img0.resize(target_size)
@@ -496,17 +644,21 @@ def image_show():
     # Create Frame
     frame1 = Frame(root, background='white')
     frame1.grid(row=2, column=2 )
-
+    
+    
     #img shown
-    my_label = Label(frame1,image = resized_images[current_index], borderwidth =0, highlightbackground="white")
+    my_label = Label(frame1,image = final_array[current_findex], borderwidth =0, highlightbackground="white")
     my_label.grid(row = 2, column = 2, sticky='ew')
+    
 
 
 # functions
 image_show()
 arrow_button()
+arrow_buttonR()
 image_function()
 root.mainloop()
 #compare the current code with past code
 #turn into exe
 # https://www.blog.pythonlibrary.org/2021/05/27/pyinstaller-how-to-turn-your-python-code-into-an-exe-on-windows/
+
